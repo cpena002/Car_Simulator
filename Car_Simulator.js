182e3b0dd01a631a76fac9dcdc74b9e5f3012995
cpena002 <carlos_enrique_pena@yahoo.com>
2016-09-30 16:58:13 -0700

    Car Simulator JS challenge 092916

---
+++ Car_Simulator.js
@@ -0,0 +1,18 @@
+// function newCar() {
+//   var info = document.getElementById("make1", "model1", "year1").value;
+//   return document. getElementById("yourcar").innerHTML = info
+// } DON'T FORGET YOUR MISTAKES!!!!
+
+
+function newCar() {
+  var info = document. getElementById("make1").value;
+  var info1 = document. getElementById("model1").value;
+  var info2 = document. getElementById("year1").value;
+  return document.
+   getElementById("yourcar").innerHTML = "Congratulations. You've created a " + info + ", " + info1 + " " + info2
+};
+
+function speed() {
+  var carSpeed = 0
+  if document. getElementById("").value;
+}
